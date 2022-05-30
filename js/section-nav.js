2a0d003115ffaf8bf7294fe24ba6a12aea1bffa4
NODUF <NitikVideos@gmail.com>
2022-05-30 15:51:21 +0200

    wipe

--- js/section-nav.js
+++
@@ -1,8 +0,0 @@
-const sectionBtn = document.querySelectorAll('.section-nav-btn')
-
-sectionBtn.forEach(button =>{
-    button.addEventListener('click', function(){
-        sectionBtn.forEach(btn => btn.classList.remove('active'));
-        this.classList.add('active');
-    })
-})
